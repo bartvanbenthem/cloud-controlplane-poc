2751a3127c1b6bc5fd8179ac2a4eb7814e624d69
bartvanbenthem <bartvanbenthem@hotmail.com>
2026-09-01 16:42:02 +0200

    update

--- packages/app/src/index.tsx
+++
@@ -1,7 +0,0 @@
-import '@backstage/cli/asset-types';
-import '@backstage/ui/css/styles.css';
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-
-ReactDOM.createRoot(document.getElementById('root')!).render(<App />);
